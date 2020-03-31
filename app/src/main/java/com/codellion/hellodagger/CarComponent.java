a7801d0d181ead5c1fda7dfdeb92df0b7e53971e
khedubaba <adityakhedekar49@gmail.com>
2020-03-31 09:51:03 +0530

    Dependency injection with Dagger 2 example.

---
+++ app/src/main/java/com/codellion/hellodagger/CarComponent.java
@@ -0,0 +1,8 @@
+package com.codellion.hellodagger;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/com/codellion/hellodagger/Wheels.java
@@ -0,0 +1,11 @@
+package com.codellion.hellodagger;
+
+import javax.inject.Inject;
+
+public class Wheels {
+
+    @Inject
+    public Wheels(){
+
+    }
+}
